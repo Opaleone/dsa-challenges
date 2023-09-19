bce2b0a3193af27d788e3e14f3663a0791b8dacc
Leone Varando <lvarando@gmail.com>
2023-09-19 08:50:58 -0500

    Solved array-intersection

--- classAlgos/array-intersection/Unsolved/array-intersection.js
+++ classAlgos/array-intersection/Unsolved/array-intersection.js
@@ -1,4 +1,20 @@
 // Write code to create a function that accepts two arrays of numbers
 // Return a new array containing the intersecting elements of the arrays
 
-var arrayIntersection = function(arr1, arr2) {};
+const arrayIntersection = (arr1, arr2) => {
+  const values = [];
+
+  let hash = {};
+
+  for (const val of arr1) {
+    hash[val] = val;
+  }
+
+  for (const val of arr2) {
+    if (val in hash) {
+      values.push(val);
+    }
+  }
+
+  return values;
+};
