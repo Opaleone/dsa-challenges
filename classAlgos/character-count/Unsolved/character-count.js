5a061407c2d251b18618459583483d210bc77374
Leone Varando <lvarando@gmail.com>
2023-09-19 14:07:02 -0500

    Completed character count algo

--- classAlgos/character-count/Unsolved/character-count.js
+++ classAlgos/character-count/Unsolved/character-count.js
@@ -1,3 +1,14 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
-var characterCount = function(str) {};
+var characterCount = function(str) {
+  const hash = {};
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] in hash) {
+      hash[str[i]] = hash[str[i]] + 1;
+    } else {
+      hash[str[i]] = 1;
+    }
+  }
+  return hash;
+};
