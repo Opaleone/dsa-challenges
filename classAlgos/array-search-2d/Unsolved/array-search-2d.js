e288fe92c84c5160d2d511b40dd5c48fb3079778
Leone Varando <lvarando@gmail.com>
2023-09-19 08:56:46 -0500

    Completed array-search-2d algo

--- classAlgos/array-search-2d/Unsolved/array-search-2d.js
+++ classAlgos/array-search-2d/Unsolved/array-search-2d.js
@@ -2,4 +2,16 @@
 // Each 2D element contains either the string "X" or "O" (both capitalized)
 // Return the number of times "X" appears in the 2D array
 
-var arraySearch2D = function(arr) {};
+var arraySearch2D = function(arr) {
+  let cnt = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].forEach((val) => {
+      if (val === 'X') {
+        cnt++;
+      }
+    })
+  }
+
+  return cnt;
+};
