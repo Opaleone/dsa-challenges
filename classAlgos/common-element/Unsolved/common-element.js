3fafdf57e2df2f055b9c6632000b5b342bae87cf
Leone Varando <lvarando@gmail.com>
2023-09-19 21:01:00 -0500

    Completed common element algo

--- classAlgos/common-element/Unsolved/common-element.js
+++ classAlgos/common-element/Unsolved/common-element.js
@@ -3,4 +3,20 @@
 // Return the common number
 // You may not use the `indexOf` or `includes` method
 
-var commonElement = function(arrA, arrB) {};
+var commonElement = function(arrA, arrB) {
+  let commonNum = arrA[0];
+
+  const hash = {};
+
+  for (let i = 0; i < arrA.length; i++) {
+    hash[arrA[i]] = true;
+  }
+
+  for (let i = 0; i < arrB.length; i++) {
+    if (hash[arrB[i]]) {
+      return arrB[i];
+    }
+  }
+
+  return commonNum;
+};
