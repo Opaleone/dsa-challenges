7f24b48ac40d2528bf72bd9d772d2abfc39d8b41
Leone Varando <lvarando@gmail.com>
2023-09-21 02:26:15 -0500

    Completed factorial algo

--- classAlgos/factorial/Unsolved/factorial.js
+++ classAlgos/factorial/Unsolved/factorial.js
@@ -1,3 +1,7 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function(num) {
+  if (num === 0) return 1;
+  num *= factorial(num - 1);
+  return num;
+};
