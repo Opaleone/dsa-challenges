77d1a21fb5c2c4e0b88ee3450a3b867fa5f68c7d
Leone Varando <lvarando@gmail.com>
2023-09-24 16:42:30 -0500

    Completed linear search algo; Included binary search algo that has better ST implications as long as array is sorted

--- classAlgos/linear-search/Unsolved/linear-search.js
+++ classAlgos/linear-search/Unsolved/linear-search.js
@@ -3,4 +3,26 @@
 // else return -1
 // You may not use the `indexOf`, `lastIndexOf` or `includes` methods
 
-var linearSearch = function(arr, target) {};
+var linearSearch = function(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+
+  return -1;
+
+// BINARY SEARCH
+  // let l = 0;
+  // let r = arr.length - 1;
+
+  // while (l < r) {
+  //   let mid = Math.floor((l + r) / 2);
+
+  //   if (arr[mid] === target) return mid;
+  //   else if (arr[mid] > target) l = mid + 1;
+  //   else r = mid - 1;
+  // }
+
+  // return -1;
+};
