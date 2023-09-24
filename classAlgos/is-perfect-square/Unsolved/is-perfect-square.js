47a4dd7d7ab8ef9db14371001188cc6683b7eb58
Leone Varando <lvarando@gmail.com>
2023-09-24 16:11:20 -0500

    Completed perfect square algo

--- classAlgos/is-perfect-square/Unsolved/is-perfect-square.js
+++ classAlgos/is-perfect-square/Unsolved/is-perfect-square.js
@@ -1,4 +1,16 @@
 // Write code to create a function that accepts a number and returns true if the number is a perfect square, otherwise it returns false.
 // You may not use the built-in Math.sqrt method
 
-var isPerfectSquare = function(num) {};
+var isPerfectSquare = function(num) {
+  let i = 0;
+
+  while (true) {
+    const square = i * i;
+
+    if (square === num) return true;
+    else if (square > num) return false;
+    else i++;
+  }
+};
+
+console.log(isPerfectSquare(9));
