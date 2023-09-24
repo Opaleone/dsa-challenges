fd279aabf9fd114392c0cd1417eb4acf6be2088f
Leone Varando <lvarando@gmail.com>
2023-09-24 15:59:31 -0500

    Completed isPalindrome algo

--- classAlgos/is-palindrome/Unsolved/is-palindrome.js
+++ classAlgos/is-palindrome/Unsolved/is-palindrome.js
@@ -1,5 +1,13 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
 var isPalindrome = function(str) {
-  return str.split('').reverse().join('') === str;
+  let reverse = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  
+  return reverse === str;
 };
+
+console.log(isPalindrome('racecar'))
