666073b0f5e0249d32f43e4d64096e6406a2ceab
Leone Varando <lvarando@gmail.com>
2023-09-24 16:15:04 -0500

    Completed is unique algo

--- classAlgos/is-unique/Unsolved/is-unique.js
+++ classAlgos/is-unique/Unsolved/is-unique.js
@@ -1,4 +1,16 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+var isUnique = function(arr) {
+  const hash = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] in hash) {
+      return false;
+    } else {
+      hash[arr[i]] = true;
+    }
+  }
+
+  return true;
+};
