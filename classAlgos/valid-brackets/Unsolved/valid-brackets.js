5bddb920ed9da53ada9cc548900f2f99a238a523
Leone Varando <lvarando@gmail.com>
2023-09-25 19:06:04 -0500

    Completed valid brackets

--- classAlgos/valid-brackets/Unsolved/valid-brackets.js
+++ classAlgos/valid-brackets/Unsolved/valid-brackets.js
@@ -1,3 +1,28 @@
 // Write a function to take a string and return true if it contains valid sets of matching brackets, else return false
 
-const validBrackets = function(str) {};
+const validBrackets = function(str) {
+  const stack = [];
+  const strNoSpace = str.replaceAll(/\s/g,'')
+  const strArr = strNoSpace.split('');
+
+  for (let i = 0; i < strArr.length; i++) {
+    switch (strArr[i]) {
+      case '(':
+        stack.push(')')
+        break;
+      case '[':
+        stack.push(']');
+        break;
+      case '{':
+        stack.push('}');
+        break;
+      default:
+        const close = stack.pop();
+        if (strArr[i] !== close) {
+          return false;
+        }
+    }
+  }
+
+  return true;
+};
