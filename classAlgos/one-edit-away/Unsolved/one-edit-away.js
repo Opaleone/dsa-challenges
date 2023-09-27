a450b582e6961fd7acdf3062f3a25928300c76a8
Leone Varando <lvarando@gmail.com>
2023-09-26 22:48:18 -0500

    Completed one edit away algo

--- classAlgos/one-edit-away/Unsolved/one-edit-away.js
+++ classAlgos/one-edit-away/Unsolved/one-edit-away.js
@@ -1,3 +1,12 @@
 const oneEditAway = function(str1, str2) {
 // TODO: Write function that takes in two strings and returns true if one character away, otherwise false
+  let charCnt = 0;
+
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i]) {
+      charCnt++;
+    }
+  }
+
+  return charCnt === 1;
 }
