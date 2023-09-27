fbfd34e76e396afc7adf74211fdf4ae88660a1a8
Leone Varando <lvarando@gmail.com>
2023-09-26 23:19:14 -0500

    Completed peak finder algo

--- classAlgos/peak-finder/Unsolved/peak-finder.js
+++ classAlgos/peak-finder/Unsolved/peak-finder.js
@@ -1,4 +1,14 @@
 const peakFinder = function(nums) {
   // TODO: Write a function that takes an array of integers containing exactly one peak.
   // A peak is defined as a location in the array where the value is greater than every number to the left and every number to the right. Return the value found at the array's peak
+
+  let peak = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > peak) {
+      peak = nums[i];
+    }
+  }
+
+  return peak;
 };
