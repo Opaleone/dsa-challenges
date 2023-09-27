a637f642e114f0283b30e96143139a06aae26259
Leone Varando <lvarando@gmail.com>
2023-09-26 22:19:54 -0500

    Completed multiply into 20 algo

--- classAlgos/multiply-into-20/Unsolved/multiply-into-20.js
+++ classAlgos/multiply-into-20/Unsolved/multiply-into-20.js
@@ -1,3 +1,13 @@
 const multiplyInto20 = function(arr) {
   // TODO: Write Your Code Here
+  const hash = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    const product = 20 / arr[i];
+
+    if (product in hash) return true;
+    else hash[arr[i]] = true;
+  }
+
+  return false;
 };
