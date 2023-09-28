983f7469c5e1ec79bc4ce1ac0beabd295970463c
Leone Varando <lvarando@gmail.com>
2023-09-27 22:27:49 -0500

    Completed permutation subStr algo

--- classAlgos/permutation-substring/Unsolved/permutation-substring.js
+++ classAlgos/permutation-substring/Unsolved/permutation-substring.js
@@ -2,4 +2,26 @@
 // Return true if the second string is a substring of any permutation of the first string
 // Else return false
 
-var permutationSubstring = function(str, sub) {};
+const str = "nAtsuiPlwuan";
+const sub = "Austin";
+
+var permutationSubstring = function(str, sub) {
+  let cnt = 0;
+  const hash = {};
+
+  for (let i = 0; i < str.length; i++) {
+    hash[str[i]] = true;
+  }
+
+  for (let i = 0; i < sub.length; i++) {
+    if (sub[i] in hash) {
+      cnt++;
+    }
+  }
+
+  return cnt === sub.length;
+};
+
+console.time('exec_time');
+console.log(permutationSubstring(str, sub));
+console.timeEnd('exec_time');
