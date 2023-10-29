35f5514fafc5ee7080e9d91b1c64f73efd7342cf
Leone Varando <lvarando@gmail.com>
2023-10-29 17:30:07 -0500

    Completed reverse no built in algo

--- classAlgos/reverse-no-built-in/Unsolved/reverse-no-built-in.js
+++ classAlgos/reverse-no-built-in/Unsolved/reverse-no-built-in.js
@@ -1,4 +1,12 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
-const reverse = (str) => {};
+const reverse = (str) => {
+  let newStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+
+  return newStr;
+};
