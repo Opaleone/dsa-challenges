4750469ef1e8a0f0e09d7663897d2149d473ccad
Leone Varando <lvarando@gmail.com>
2023-11-04 18:36:03 -0500

    Completed smallest difference algo

--- classAlgos/smallest-difference/Unsolved/smallest-difference.js
+++ classAlgos/smallest-difference/Unsolved/smallest-difference.js
@@ -3,4 +3,51 @@
 
 // TODO: Solve the function below:
 
-var smallestDifference = function (arr1, arr2) {};
+
+// My initial sol
+// var smallestDifference = function (arr1, arr2) {
+//   let smallest = Infinity;
+//   let newArr = [];
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     for (let j = 0; j < arr2.length; j++) {
+//       const curDiff = Math.abs(arr1[i] - arr2[j]);
+
+//       if (curDiff < smallest) {
+//         smallest = curDiff;
+//         newArr = [arr1[i], arr2[j]];
+//       }
+//     }
+//   }
+
+//   return newArr;
+// };
+
+
+// More efficient sol
+var smallestDifference = function (arr1, arr2) {
+  let bestDiff = Infinity;
+  let newArr = [];
+  let l = 0;
+  let r = 0;
+
+  while (l < arr1.length && r < arr2.length) {
+    const curDiff = Math.abs(arr1[l] - arr2[r]);
+    if (curDiff < bestDiff) {
+      bestDiff = curDiff;
+      newArr = [arr1[l], arr2[r]];
+
+      if (bestDiff === 0) {
+        return newArr;
+      }
+    }
+
+    if (arr1[l] < arr2[r]) {
+      l++;
+    } else if (arr2[r] < arr1[l]) {
+      r++;
+    }
+  }
+
+  return newArr;
+};
