ea44f8251eed8c02cc167c043fd230c2a13ee0f7
Leone Varando <lvarando@gmail.com>
2023-11-11 11:35:21 -0600

    Completed camelCase algo

--- classAlgos/camel-case/Unsolved/camel-case.js
+++ classAlgos/camel-case/Unsolved/camel-case.js
@@ -1,3 +1,19 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
-var camelCase = function(str) {};
+var camelCase = function(str) {
+  const strArr = str.toLowerCase().split(' ');
+  let camelCase = "";
+
+  for (let i = 0; i < strArr.length; i++) {
+    const word = strArr[i];
+    const letters = word.split('');
+
+    if (i > 0) {
+      letters[0] = letters[0].toUpperCase();
+    }
+
+    camelCase += letters.join('');
+  }
+
+  return camelCase;
+};
