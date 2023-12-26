49a0fbec7630405be1cdce1f2a109c083806bbe6
Leone Varando <lvarando@gmail.com>
2023-12-25 18:18:12 -0600

    Pushing all changes

---
+++ generalStudy/setPrac.js
@@ -0,0 +1,13 @@
+let set = new Set();
+
+for (let i = 0; i < 100; i++) {
+  set.add(i);
+};
+
+// set.forEach((entry) => {
+//   console.log(entry);
+// });
+
+console.log(set.entries());
+
+// console.log(set)
