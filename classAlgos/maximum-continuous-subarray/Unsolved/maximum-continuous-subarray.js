5def9bc4572c820fc816259e7046bad527c42347
Leone Varando <lvarando@gmail.com>
2024-03-11 18:32:51 -0500

    Completed algo

--- classAlgos/maximum-continuous-subarray/Unsolved/maximum-continuous-subarray.js
+++ classAlgos/maximum-continuous-subarray/Unsolved/maximum-continuous-subarray.js
@@ -1,3 +1,18 @@
-const maxSumArray = function(numbers) {
+const maxSumArray = function(nums) {
   // Write your solution here
+  if (nums.length === 1) return nums[0];
+
+  if (nums.length === 2 || nums.every((x) => x >= 0)) return nums.reduce((a, b) => a + b, 0);
+
+  let curMax = nums[0];
+  let maxSum = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    let curNum = nums[i];
+
+    curMax = Math.max(curNum, curMax + curNum);
+    maxSum = Math.max(curMax, maxSum);
+  }
+
+  return maxSum;
 };
