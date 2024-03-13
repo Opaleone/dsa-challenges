706fa47dde74b2b5b7ccaa2d051408908e7bcb16
Leone Varando <lvarando@gmail.com>
2024-03-13 18:03:43 -0500

    Completed algo

--- classAlgos/rotation-point/Unsolved/rotation-point.js
+++ classAlgos/rotation-point/Unsolved/rotation-point.js
@@ -1,3 +1,20 @@
 // Write a function that takes in an array of sorted strings and returns the index of its rotation point if it has one, else return -1
 
-var rotationPoint = function(words) {};
+var rotationPoint = function(words) {
+  let l = 0;
+  let r = words.length - 1;
+
+  while (l <= r) {
+    const mid = Math.floor((l+r) / 2);
+
+    if (words[mid] < words[mid - 1]) return mid;
+
+    if (words[l] > words[mid]) {
+      r = mid - 1;
+    } else {
+      l = mid + 1;
+    }
+  }
+
+  return -1;
+};
