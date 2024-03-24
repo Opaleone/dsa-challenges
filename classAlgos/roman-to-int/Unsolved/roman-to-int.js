ebbeff7f421e0d9297d4a8ce8bc01bd816f9cb6f
Leone Varando <lvarando@gmail.com>
2024-03-23 19:45:33 -0500

    Completed algo

--- classAlgos/roman-to-int/Unsolved/roman-to-int.js
+++ classAlgos/roman-to-int/Unsolved/roman-to-int.js
@@ -1,3 +1,26 @@
 // Write a function that takes in a Roman Numeral string and returns its integer form
 
-var romanToInt = function(str) {};
+var romanToInt = function(s) {
+  const romanObj = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  }
+
+  let num = 0;
+
+  for (let i = 0; i < s.length; i++) {
+      if (romanObj[s[i]] < romanObj[s[i + 1]]) {
+          num -= romanObj[s[i]];
+      } else {
+          num += romanObj[s[i]]
+      }
+
+  }
+
+  return num;
+};
