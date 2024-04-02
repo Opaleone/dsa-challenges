883a210cfc89eace2acf8743b9b77de63a133482
Leone Varando <lvarando@gmail.com>
2024-04-02 18:06:05 -0500

    Completed isPalindrome'

--- algoExpert/isPalindrome.js
+++ algoExpert/isPalindrome.js
@@ -8,8 +8,7 @@ let string = 'racecar';
 // Solution 1
 //    T: O(n^2) S: O(n)
 // function isPalindrome(string) {
-//   // Write your code here.
-//   let reverse = '';
+//   let reverse = ''
 
 //   for (let i = string.length - 1; i >= 0; i--) {
 //     reverse += string[i];
@@ -22,29 +21,29 @@ let string = 'racecar';
 //    Sliding window algo
 //      T: O(n) S: O(1)
 
-function isPalindrome(string) {
-  let leftIdx = 0;
-  let rightIdx = string.length - 1;
+// function isPalindrome(string) {
+//   let leftIdx = 0;
+//   let rightIdx = string.length - 1;
 
-  while (leftIdx < rightIdx) {
-    if (string[leftIdx] !== string[rightIdx]) return false;
-    leftIdx++;
-    rightIdx--;
-  }
+//   while (leftIdx < rightIdx) {
+//     if (string[leftIdx] !== string[rightIdx]) return false;
+//     leftIdx++;
+//     rightIdx--;
+//   }
 
-  return true;
-}
+//   return true;
+// }
 
 // Solution 3
 //    Double var in for loop
-//      T: O(n)  S: O(n)
+//      T: O(n)  S: O(1)
 
-// function isPalindrome(string) {
-//   for (let i = 0, j = string.length - 1; i < string.length; i++, j--) {
-//     if (string[i] !== string[j]) return false;
-//   }
-//   return true;
-// }
+function isPalindrome(string) {
+  for (let i = 0, j = string.length - 1; i < string.length; i++, j--) {
+    if (string[i] !== string[j]) return false;
+  }
+  return true;
+}
 
 // Solution 4 - BAD
 // NOT OPTIMAL DUE TO METHOD USAGE VASTLY INCREASING ST COMPLEXITY
